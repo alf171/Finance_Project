0fcac3a511d4125e7e41b36d3b055195b21459cf
alf171 <alfonso.laffont@gmail.com>
2020-11-15 16:52:37 -0500

    Add files via upload

---
+++ Investing.py
@@ -0,0 +1,18 @@
+#https://www.sec.gov/search/search.htm
+
+#**BALANCE SHEET** - Snapshot of time
+#Assets = Cash, Accounts Recievable, Inventory, PP&E (to an extent), other assets
+#Libailities = Accounts Payable, Debt, Employees, Accrued Expenses, Deferred Revenue
+#Share holders Equity / Book Value -> S/E = (A - L)
+
+#**INCOME STATEMENT** - Period of time
+#Revenue/Sales/Turnover
+#Costs/Operating expenses = R/D expense + Admistrative expense + Advertising + ...
+#Profits/Income = Revinue - Costs
+#Margin of Profit -> Profit/Revenue -> Different kinds of companies can have different amounts 
+
+#Cash flow statment 
+#
+#
+
+
